9c7eb1fc161e50f45f7ff60ea014bbf2ddde4e2e
ClearT <clear1014@gmail.com>
2020-09-17 15:32:30 +0900

    version 1.0

---
+++ ko/js/client.js
@@ -0,0 +1,17 @@
+
+var imagelinks={
+    'eaz':'/ko/img/eaz-map-destiny-2.jpg',
+    'leviathan':'/ko/img/leviathan.png',
+    'leviathangarden':'/ko/img/leviathangarden.webp',
+    'pitofheresy1':'/ko/img/pitofheresy1.jpg',
+    'pitofheresy2':'/ko/img/pitofheresy2.jpg',
+    'scourgeofthepast':'/ko/img/scourgeofthepast.webp',
+    'shatteredthrone':'/ko/img/shatteredthrone.jpg',
+    'zerohour':'https://i.imgur.com/BAqFBHY.png',
+    'leviathangarden_prestige':'/ko/img/leviathangarden_presitge.jpg'
+}
+
+function changeImage(type){
+    var newImage=$('<img src='+imagelinks[type]+'></img>')
+    $('#ImgContainer > img').replaceWith(newImage);
+}
